3b71a8438e6564e4ab245258f1113cb4add92f88
Yuval Abiri <Yuval.Abiri@post.runi.ac.il>
2023-12-22 13:23:08 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,9 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class AddTwo{
+
+    public static void main(String[]args) {
+        int num1 = Integer.parseInt(args[0]);       
+        int num2 = Integer.parseInt(args[1]);
+            System.out.println(args[0] + " " + "+" + " " + args[1] + " " + "=" + " " + (num1 + num2)); 
+        } 
+
 }
--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,19 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Gen3{
+
+    public static void main(String[]args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int p = (int)(Math.random() * (b - a) + a);
+        int q = (int)(Math.random() * (b - a) + a);
+        int r = (int)(Math.random() * (b - a) + a);
+            System.out.println(p);
+            System.out.println(q);
+            System.out.println(r);
+            int minPQ = Math.min(p, q);
+            int minPR = Math.min(minPQ, r);
+              System.out.println("The minimal generated number was" + " " + minPR);
+        
+            
+        }
+
 }
