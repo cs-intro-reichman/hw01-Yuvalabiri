2be7fe60cec3c9cc5cc7679287433b72f6a32082
Yuval Abiri <Yuval.Abiri@post.runi.ac.il>
2023-12-22 16:41:27 +0200

    add awesome code for Homework

--- Coins.java
+++ Coins.java
@@ -2,7 +2,7 @@ public class Coins{
 
     public static void main(String[]args) {
         int num = Integer.parseInt(args[0]);       
-            System.out.println("Use" + " " + (num/25) + " " + "quarters" + " " + "and" + " " + (num%25)); 
+            System.out.println("Use" + " " + (num/25) + " " + "quarters" + " " + "and" + " " + (num%25) + " " + "cents"); 
         } 
 
 }
